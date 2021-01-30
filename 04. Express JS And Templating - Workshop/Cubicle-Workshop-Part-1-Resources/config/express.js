1d0185cc236946195387b0cb3db99205c5aeb364
Krasi Zorbov <krasi.zorbov@abv.bg>
2021-01-30 21:00:01 +0200

    Added Body-Parser

--- 04. Express JS And Templating - Workshop/Cubicle-Workshop-Part-1-Resources/config/express.js
+++ 04. Express JS And Templating - Workshop/Cubicle-Workshop-Part-1-Resources/config/express.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser');
 
 module.exports = (app) => {
     
+    //TODO: Setup the view engine
     app.engine('handlebars', handlebars());
     app.set('view engine', 'handlebars');
     app.use(express.static('static'));
-    //TODO: Setup the view engine
+    
 
     //TODO: Setup the body parser
+    app.use(bodyParser.urlencoded({ extended: false }));
 
     //TODO: Setup the static files
 
