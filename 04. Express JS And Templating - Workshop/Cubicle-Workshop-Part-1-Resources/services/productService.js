25da0c4c0645b11acab85d5aa81223421bcc05e6
Krasi Zorbov <krasi.zorbov@abv.bg>
2021-01-31 13:12:32 +0200

    Refactoring

--- 04. Express JS And Templating - Workshop/Cubicle-Workshop-Part-1-Resources/services/productService.js
+++ 04. Express JS And Templating - Workshop/Cubicle-Workshop-Part-1-Resources/services/productService.js
@@ -1,6 +1,6 @@
 const Cube = require('../models/product');
 const uniqid = require('uniqid');
-const fs = require('fs');
+const fs = require('fs').promises;
 let products = require('../products.json');
 
 function getAll(){
@@ -13,16 +13,19 @@ function getById(id){
 
 async function create(data){
     let cube = new Cube(uniqid(), data.name, data.description, data.imageUrl, data.difficultyLevel);
-
     products.push(cube);
-
     // Use the "path" library to configure the path with "path.join" or "path.resolve"
-    await fs.writeFile('products.json', JSON.stringify(products), (err) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-    });
+    try {
+        await fs.writeFile('products.json', JSON.stringify(products));
+    } catch (error) {
+        console.log(error);
+    }
+    // await fs.writeFile('products.json', JSON.stringify(products), (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+    // });
 }
 
 module.exports = {create, getAll, getById};
