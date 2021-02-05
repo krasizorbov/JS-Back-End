470d06ad1e9b6adb1d33f4df794694648827b1b0
Krasi Zorbov <krasi.zorbov@abv.bg>
2021-02-05 15:30:55 +0200

    refactoring

--- JS Back End Exam Cat Shelter/utils/loginValidator.js
+++ JS Back End Exam Cat Shelter/utils/loginValidator.js
@@ -9,5 +9,5 @@ module.exports = [
           }
         });
       }),
-    body('password', 'Password must be at leas 6 characters long').isLength({min: 6})
+    body('password', 'Password must be at least 6 characters long').isLength({min: 6})
 ]
--- JS Back End Exam Cat Shelter/utils/registerValidator.js
+++ JS Back End Exam Cat Shelter/utils/registerValidator.js
@@ -3,7 +3,7 @@ const { body } = require('express-validator');
 module.exports = [
     body('email', "The provided email is not valid!").isEmail(),
     body('fullName', 'The name must be at least 5 characters long').isLength({min: 5}),
-    body('password', 'Password must be at leas 6 characters long').isLength({min: 6}),
+    body('password', 'Password must be at least 6 characters long').isLength({min: 6}),
     body('repeatPassword').custom(passwordCheck)
 ]
 
