26656bd2e01e8fcac6abcc7513758a6d73d56505
Krasi Zorbov <krasi.zorbov@abv.bg>
2021-02-05 17:52:28 +0200

    refactoring

--- JS Back End Exam Cat Shelter/utils/loginValidator.js
+++ JS Back End Exam Cat Shelter/utils/loginValidator.js
@@ -3,11 +3,11 @@ const { User } = require('../models');
 
 module.exports = [
   body('email', "The provided email is not valid!").isEmail().custom(value => {
-        return User.findOne({email: value}).then(user => {
-          if (!user) {
-            return Promise.reject('E-mail does not exist!');
-          }
-        });
+    return User.findOne({email: value}).then(user => {
+      if (!user) {
+        return Promise.reject('E-mail does not exist!');
+      }
+    });
       }),
-    body('password', 'Password must be at least 6 characters long').isLength({min: 6})
+  body('password', 'Password must be at least 6 characters long').isLength({min: 6})
 ]
