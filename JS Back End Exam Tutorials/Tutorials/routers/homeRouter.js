176527ba2619997e43b8b2c39c2f2d2cd2a8d90e
Krasi Zorbov <krasi.zorbov@abv.bg>
2021-02-12 17:20:40 +0200

    Refactoring

--- JS Back End Exam Tutorials/Tutorials/routers/homeRouter.js
+++ JS Back End Exam Tutorials/Tutorials/routers/homeRouter.js
@@ -1,5 +1,4 @@
 const { homeController } = require('../controllers');
-const isAuthenticated = require('../middlewares/isAuthenticated');
 
 module.exports = (router) => {
     router.get('/', homeController.get.home);
