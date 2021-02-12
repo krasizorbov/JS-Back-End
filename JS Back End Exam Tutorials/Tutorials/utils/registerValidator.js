c506437a1d5a9ca6d944509e5eaf87b2b1580bfe
Krasi Zorbov <krasi.zorbov@abv.bg>
2021-02-12 14:53:05 +0200

    Refactoring

--- JS Back End Exam Tutorials/Tutorials/utils/registerValidator.js
+++ JS Back End Exam Tutorials/Tutorials/utils/registerValidator.js
@@ -2,14 +2,19 @@ const { body } = require('express-validator');
 const { User } = require('../models');
 
 module.exports = [
-    body('username', "The provided username is not valid!").isLength({min: 5}).custom(username => {
+    body('username')
+    .isLength({min: 5}).withMessage("Username must be at least 5 characters long!")
+    .matches(/\d/).withMessage('Username must contain a number!')
+    .custom(username => {
         return User.findOne({username: username}).then(user => {
           if (user) {
             return Promise.reject('User already exist!');
           }
         });
       }),
-    body('password', 'Password must be at least 6 characters long').isLength({min: 6}),
+    body('password')
+    .isLength({min: 5}).withMessage("Password must be at least 5 characters long!")
+    .matches(/\d/).withMessage('Password must contain a number!'),
     body('repeatPassword').custom(passwordCheck)
 ]
 
